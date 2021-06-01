cb547cec8c003f2755379a8722fd5842ab7aaa46
Konstantin Pelz <konstantin.pelz@online.de>
2021-06-01 23:01:43 +0200

    autogenes continued

--- R/AutoGeneS.R
+++ R/AutoGeneS.R
@@ -16,7 +16,7 @@ build_model_autogenes <- function(single_cell_object, cell_type_annotations, bul
   path_to_build_model_python_script <- paste0(here(),"/R/train_model.py")
   parameters <- get_buildmodel_parameters()
 
-  system(paste("python3",path_to_build_model_python_script,parameters), ignore.stdout = !verbose, ignore.stderr = !verbose)
+  system(paste("python3",path_to_build_model_python_script,"-sc",saved_h5ad,"-out autogenes.pickle",parameters), ignore.stdout = !verbose, ignore.stderr = !verbose)
   return()
 }
 
@@ -68,6 +68,41 @@ autogenes_checkload <- function(){
   reticulate::import("autogenes")
 }
 
-get_buildmodel_parameters <- function(){
+get_buildmodel_parameters_autogenes <- function(ngen=2,mode="standard",nfeatures=NULL,weights=c(-1,1),objectives=c("correlation","distance"),seed=0,verbose=FALSE,population_size=100,
+                                      offspring_size=50,crossover_pb=0.7,mutation_pb=0.3,mutate_flip_pb=1E-3,crossover_thres=1000,ind_standard_pb=0.1,plot_weights=NULL,
+                                      plot_objectives=c(0,1),index=NULL,close_to=NULL,plot=FALSE){
+  string <- paste("-ngen",ngen,"-mode",mode,"-nfeatures",nfeatures,"-weights",paste(weights,collapse = " "),"-objectives",paste(objectives,collapse = " "),"-seed",seed,
+                  "-verbose",verbose,"-population_size",population_size,"-offspring_size",offspring_size,"-crossover_pb",crossover_pb,"-mutation_pb",mutation_pb,
+                  "-mutate_flip_pb",mutate_flip_pb,"-crossover_thres",crossover_thres,"-ind_standard_pb",ind_standard_pb,"-plot",plot,"-plot_objectives",
+                  paste(plot_objectives,collapse = " "))
+  if (plot_weights){
+    string <- paste(string,"-plot_weights",paste(plot_weights,collapse = " "))
+  }
+  if (index){
+    string <- paste(string,"-index",paste(index,collapse = " "))
+  }
+  if (close_to){
+    string <- paste(string,"-close_to",paste(close_to,collapse = " "))
+  }
+  return(string)
+}
 
+
+get_deconvolute_parameters_autogenes <- function(ngen=2,mode="standard",nfeatures=NULL,weights=c(-1,1),objectives=c("correlation","distance"),seed=0,verbose=FALSE,population_size=100,
+                                      offspring_size=50,crossover_pb=0.7,mutation_pb=0.3,mutate_flip_pb=1E-3,crossover_thres=1000,ind_standard_pb=0.1,plot_weights=NULL,
+                                      plot_objectives=c(0,1),index=NULL,close_to=NULL,plot=FALSE){
+  string <- paste("-ngen",ngen,"-mode",mode,"-nfeatures",nfeatures,"-weights",paste(weights,collapse = " "),"-objectives",paste(objectives,collapse = " "),"-seed",seed,
+                  "-verbose",verbose,"-population_size",population_size,"-offspring_size",offspring_size,"-crossover_pb",crossover_pb,"-mutation_pb",mutation_pb,
+                  "-mutate_flip_pb",mutate_flip_pb,"-crossover_thres",crossover_thres,"-ind_standard_pb",ind_standard_pb,"-plot",plot,"-plot_objectives",
+                  paste(plot_objectives,collapse = " "))
+  if (plot_weights){
+    string <- paste(string,"-plot_weights",paste(plot_weights,collapse = " "))
+  }
+  if (index){
+    string <- paste(string,"-index",paste(index,collapse = " "))
+  }
+  if (close_to){
+    string <- paste(string,"-close_to",paste(close_to,collapse = " "))
+  }
+  return(string)
 }

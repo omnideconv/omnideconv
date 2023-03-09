a3684d2f5bd34cbf1f9f74acd99722045df63162
Merotto <c1041161@uibk.ac.at>
2023-03-09 12:34:24 +0100

    addition of load file

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+#' Manage python dependencies
+#' according to: https://rstudio.github.io/reticulate/articles/python_dependencies.html#manual-configuration
+#'
+#'
+#' @name fix_dependencies
+NULL
+
+.onLoad <- function(libname, pkgname) {
+  reticulate::configure_environment(pkgname)
+}

8b8927ba9ae8df068d9a3a97bdafee64b76acc51
Merotto <c1041161@uibk.ac.at>
2023-03-09 14:16:24 +0100

    reduce tests to only python checks

---
+++ tests/testthat/test.R
@@ -0,0 +1,7 @@
+
+test <- function() {
+  # this should return TRUE after 'pandas' is automagically installed
+  reticulate::py_module_available("anndata")
+  reticulate::py_module_available('scaden')
+  reticulate::py_module_available('autogenes')
+}

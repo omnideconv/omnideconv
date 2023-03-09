97bd8e028331773d5e9da51c57154896c2656e1b
Merotto <c1041161@uibk.ac.at>
2023-03-09 11:19:21 +0100

    removing checkload functions

--- R/Scaden.R
+++ R/Scaden.R
@@ -51,8 +51,8 @@ build_model_scaden <- function(single_cell_object, cell_type_annotations, bulk_g
   }
 
   # check if Scaden is installed and loaded.
-  scaden_checkload()
-
+  #scaden_checkload()
+  reticulate::import("scaden")
   single_cell_object <- t(single_cell_object)
   if (nrow(single_cell_object) != length(cell_type_annotations)) {
     stop(
@@ -117,7 +117,8 @@ deconvolute_scaden <- function(signature, bulk_gene_expression, temp_dir = NULL,
     }
   }
 
-  scaden_checkload()
+  #scaden_checkload()
+  reticulate::import("scaden")
   prediction <- scaden_predict(signature, bulk_gene_expression, temp_dir = temp_dir, verbose = verbose)
 
   return(t(prediction))

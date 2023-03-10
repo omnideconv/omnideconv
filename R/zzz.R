3967937f3bb315f5bacb9e6ff3302b6a536f1e9f
Merotto <c1041161@uibk.ac.at>
2023-03-10 17:23:21 +0100

    update onload function

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,18 @@
 NULL
 
 .onLoad <- function(libname, pkgname) {
-  reticulate::py_config()
+  if (!dir.exists(reticulate::miniconda_path())) {
+    message("Setting python version in miniconda to be 3.9")
+    Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.9)
+    message("Setting up miniconda environment..")
+    suppressMessages(reticulate::install_miniconda())
+  }
+
+  if (!dir.exists(reticulate::conda_python('r-reticulate'))){
+    reticulate::conda_create(envname = "r-reticulate")
+  }
+
+  reticulate::use_miniconda(condaenv = "r-reticulate", required = TRUE)
+  config <- reticulate::py_config()
   reticulate::configure_environment(pkgname, force = TRUE)
 }

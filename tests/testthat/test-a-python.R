72e3dbb41ed5caec5dd12345ef5e1e2ea258222d
Merotto <c1041161@uibk.ac.at>
2023-03-22 11:43:01 +0100

    Addition of final tests

--- tests/testthat/test-a-python.R
+++ tests/testthat/test-a-python.R
@@ -32,34 +32,34 @@ test_that("Python module autogenes is available", {
 })
 
 
-
-
-bulk_small <- as.matrix(utils::read.csv("small_test_data/bulk_small.csv", row.names = 1))
-# bulk_small <- bulk_small[, 1, drop = FALSE]
-sc_object_small <- as.matrix(utils::read.csv("small_test_data/sc_object_small.csv", row.names = 1))
-cell_annotations_small <- readr::read_lines("small_test_data/cell_annotations_small.txt")
-batch_ids_small <- readr::read_lines("small_test_data/batch_ids_small.txt")
-marker_genes <- readr::read_lines("small_test_data/marker_genes_small.txt")
-
-markers_small <- list(marker_genes[1:9], marker_genes[10:14], marker_genes[15:20])
-names(markers_small) <- sort(unique(cell_annotations_small))
-
-
-test_that("Scaden build model works", {
-  reticulate::use_miniconda(condaenv = "r-reticulate", required = TRUE)
-  model <- build_model(sc_object_small, cell_annotations_small,
-    method = "scaden",
-    bulk_gene_expression = bulk_small, samples = 10, cells = 5,
-    steps = 150, verbose = F
-  )
-  expect_equal(
-    info = "model folder is created and model assets are written",
-    object = length(list.dirs(model, recursive = F)), expected = 3
-  )
-})
-
-test_that("AutoGeneS build model works", {
-  reticulate::use_miniconda(condaenv = "r-reticulate", required = TRUE)
-  model <- build_model(sc_object_small, cell_annotations_small, "autogenes")
-  expect_true(file.exists(model), "pickle file was created successfully")
-})
+#
+#
+# bulk_small <- as.matrix(utils::read.csv("small_test_data/bulk_small.csv", row.names = 1))
+# # bulk_small <- bulk_small[, 1, drop = FALSE]
+# sc_object_small <- as.matrix(utils::read.csv("small_test_data/sc_object_small.csv", row.names = 1))
+# cell_annotations_small <- readr::read_lines("small_test_data/cell_annotations_small.txt")
+# batch_ids_small <- readr::read_lines("small_test_data/batch_ids_small.txt")
+# marker_genes <- readr::read_lines("small_test_data/marker_genes_small.txt")
+#
+# markers_small <- list(marker_genes[1:9], marker_genes[10:14], marker_genes[15:20])
+# names(markers_small) <- sort(unique(cell_annotations_small))
+#
+#
+# test_that("Scaden build model works", {
+#   reticulate::use_miniconda(condaenv = "r-reticulate", required = TRUE)
+#   model <- build_model(sc_object_small, cell_annotations_small,
+#     method = "scaden",
+#     bulk_gene_expression = bulk_small, samples = 10, cells = 5,
+#     steps = 150, verbose = F
+#   )
+#   expect_equal(
+#     info = "model folder is created and model assets are written",
+#     object = length(list.dirs(model, recursive = F)), expected = 3
+#   )
+# })
+#
+# test_that("AutoGeneS build model works", {
+#   reticulate::use_miniconda(condaenv = "r-reticulate", required = TRUE)
+#   model <- build_model(sc_object_small, cell_annotations_small, "autogenes")
+#   expect_true(file.exists(model), "pickle file was created successfully")
+# })

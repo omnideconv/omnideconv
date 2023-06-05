17794549009e39ab6a62104c56326a1a784683b7
Alexander Dietrich <adietrich@norm.exbio.wzw.tum.de>
2023-06-05 10:17:40 +0200

    skipping python dependency check for benchmark

--- R/zzz.R
+++ R/zzz.R
@@ -5,36 +5,6 @@
 NULL
 
 .onLoad <- function(libname, pkgname) {
-  # We ensure to have reticulate
-  if (!dir.exists(reticulate::miniconda_path())) {
-    message("Setting python version in miniconda to be 3.8")
-    Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.8)
-    message("Setting up miniconda environment..")
-    suppressMessages(reticulate::install_miniconda())
-  }
-
-
-  # We ensure to have the r-reticulate env
-  # if (!file.exists(reticulate::conda_python("r-reticulate"))) {
-  if (!("r-omnideconv" %in% reticulate::conda_list()$name)) {
-    reticulate::conda_create(envname = "r-omnideconv")
-  }
-
-  paths <- reticulate::conda_list()
-  path <- paths[paths$name == "r-omnideconv", 2]
-  if (.Platform$OS.type == "windows") {
-    path <- gsub("\\\\", "/", path)
-  }
-  path.bin <- gsub("/envs/omnideconv/python.exe", "/library/bin", path)
-  Sys.setenv(PATH = paste(path.bin, Sys.getenv()["PATH"], sep = ";"))
-  Sys.setenv(RETICULATE_PYTHON = path)
-
-
-  reticulate::use_miniconda(condaenv = "r-omnideconv", required = TRUE)
-  reticulate::py_config()
-  reticulate::configure_environment(pkgname, force = TRUE)
-
-  if (!reticulate::py_module_available("anndata")) {
-    anndata::install_anndata()
-  }
+  #no installatins have to be checked when running omnideconv inside the Docker container
+  message('Skipping python installation checks ...')
 }
